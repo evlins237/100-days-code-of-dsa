c48a56cb677b4e4d2eccd1b8b14e60f4feba51bb
Evlin s <evlins237@gmail.com>
2026-04-30 23:14:29 +0530

    Implement binary tree from array and in-order traversal

---
+++ Day43Q1DSA.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node *left, *right;
+} Node;
+
+Node* newNode(int val) {
+    Node* node = (Node*)malloc(sizeof(Node));
+    node->data = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    if (n == 0 || arr[0] == -1) {
+        return 0;
+    }
+
+    Node* nodes[n];
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != -1)
+            nodes[i] = newNode(arr[i]);
+        else
+            nodes[i] = NULL;
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (nodes[i] != NULL) {
+            int l = 2*i + 1;
+            int r = 2*i + 2;
+
+            if (l < n)
+                nodes[i]->left = nodes[l];
+            if (r < n)
+                nodes[i]->right = nodes[r];
+        }
+    }
+
+    Node* root = nodes[0];
+
+    Node* stack[n];
+    int top = -1;
+    Node* curr = root;
+
+    while (curr != NULL || top != -1) {
+        while (curr != NULL) {
+            stack[++top] = curr;
+            curr = curr->left;
+        }
+        curr = stack[top--];
+        printf("%d ", curr->data);
+        curr = curr->right;
+    }
+
+    return 0;
+}
