5c3c962185429d8bcef4cd3992aff94b7f9e90c7
Evlin s <evlins237@gmail.com>
2026-04-30 23:45:18 +0530

    Implement binary tree creation and level order traversal

---
+++ Day46Q1DSA.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node *left, *right;
+} Node;
+
+Node* newNode(int val) {
+    Node* node = (Node*)malloc(sizeof(Node));
+    node->data = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    if (n == 0 || arr[0] == -1)
+        return 0;
+
+    Node* nodes[n];
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != -1)
+            nodes[i] = newNode(arr[i]);
+        else
+            nodes[i] = NULL;
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (nodes[i] != NULL) {
+            int l = 2*i + 1;
+            int r = 2*i + 2;
+            if (l < n) nodes[i]->left = nodes[l];
+            if (r < n) nodes[i]->right = nodes[r];
+        }
+    }
+
+    Node* root = nodes[0];
+
+    Node* queue[n];
+    int front = 0, rear = 0;
+
+    queue[rear++] = root;
+
+    while (front < rear) {
+        Node* curr = queue[front++];
+        printf("%d ", curr->data);
+
+        if (curr->left)
+            queue[rear++] = curr->left;
+        if (curr->right)
+            queue[rear++] = curr->right;
+    }
+
+    return 0;
+}
