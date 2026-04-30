cab3618aff23a5cdcdb71351f970fd81d830623b
Evlin s <evlins237@gmail.com>
2026-05-01 00:17:07 +0530

    Implement binary tree mirror check in C

---
+++ Day56Q1DSA.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node *left, *right;
+} Node;
+
+Node* newNode(int val) {
+    Node* node = (Node*)malloc(sizeof(Node));
+    node->data = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+int isMirror(Node* a, Node* b) {
+    if (a == NULL && b == NULL) return 1;
+    if (a == NULL || b == NULL) return 0;
+    if (a->data != b->data) return 0;
+    return isMirror(a->left, b->right) && isMirror(a->right, b->left);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    if (n == 0 || arr[0] == -1) {
+        printf("YES");
+        return 0;
+    }
+
+    Node* nodes[n];
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != -1)
+            nodes[i] = newNode(arr[i]);
+        else
+            nodes[i] = NULL;
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (nodes[i] != NULL) {
+            int l = 2*i + 1;
+            int r = 2*i + 2;
+            if (l < n) nodes[i]->left = nodes[l];
+            if (r < n) nodes[i]->right = nodes[r];
+        }
+    }
+
+    if (isMirror(nodes[0], nodes[0]))
+        printf("YES");
+    else
+        printf("NO");
+
+    return 0;
+}
